12f84c1f9c14b36e5fc1fb97e807ecc947104ebd
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-08-19 00:51:54 +0300

    feat: add part Lesson 9

--- Lesson_9/ViewController.swift
+++ Lesson_9/ViewController.swift
@@ -14,6 +14,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+//    переход на предыдущий экран
+    @IBAction func backButtonPressed() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+//    переход сразу на первый экран
+    @IBAction func backToFirstButtonPressed() {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+//    переход на конкретный экран
+    @IBAction func backToSecondButtonPressed() {
+//        экраны хранятся в виде массива(viewControllers (индекс-[1])), достаем экран по индексу
+        let secondViewController = navigationController?.viewControllers[1]
+//        далее указывает этот экран с "!" знаком
+        navigationController?.popToViewController(secondViewController!, animated: true)
+    }
 
 }
 
